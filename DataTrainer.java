eefb8ca024516252c402c899f4591e604474d705
Michael Boc <mikeboc@me.com>
2017-12-22 12:43:13 -0500

    Initial creatation of the DataTrainer class

---
+++ DataTrainer.java
@@ -0,0 +1,29 @@
+/**
+ * @author       Michael Boc <mikeboc@me.com>
+ * @since        12/21/2017
+ *
+ * This class parses the raw SQL data to train the logrithmic equation
+*/
+
+
+
+import java.util.Hashtable;
+
+
+
+public class DataTrainer {
+
+    // DataTrainer Variables
+    private float b0;
+    private float b1;
+    private float b2;
+
+    // Initialize base values
+    public DataTrainer() {
+        this.b0 = 0;
+        this.b1 = 0;
+        this.b2 = 0;
+    }
+
+
+}
