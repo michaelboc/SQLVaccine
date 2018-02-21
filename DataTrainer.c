0d7da9db31acbc36af58d6f1584b92c7534679ac
Michael Boc <mhb5918@rit.edu>
2018-02-21 11:31:28 -0500

    Changed the language of this project. Going to write it in C instead.

---
+++ DataTrainer.c
@@ -0,0 +1,29 @@
+//
+// Author:      Michael Boc <mikeboc@me.com>
+// Version:     Feb-21-2018
+// Filename:    DataTrainer.c
+//
+// This source file contains the implementation of a program, which will recieve
+// training data populate a database, and use that database to generate
+// parameters for the logarithmic regression formula.
+/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
+
+
+
+// This function reads a file with training data, and then uses the data to
+// update a mySQL database. The training data should be examples of SQL
+// injection techniques.
+//
+// Param:   dataFile        A file path to the SQL training data
+// Param:   dataBase        //TODO 
+void readTrainData( char* dataFile ){
+
+}
+
+
+// Main function //TODO
+int main( int argc, char* argv[] ){
+
+
+
+}
